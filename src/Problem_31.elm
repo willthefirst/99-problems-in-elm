f725046675b0ebad058a08c8801b1db2bcd69d96
Will Moritz <will@crowdfavorite.com>
2020-08-16 12:40:10 -0400

    imperfect solution to problem_31, moving on anyway

--- src/Problem_31.elm
+++ src/Problem_31.elm
@@ -1,25 +1,37 @@
 module Problem_31 exposing (..)
+
 -- https://johncrane.gitbooks.io/ninety-nine-elm-problems/content/p/p31.html
+-- Works, but not for big numbers.
 
 import Html
+
 
 isPrime : Int -> Bool
 isPrime n =
     let
-        nums = List.range 2 n
+        nums =
+            List.range 2 n
     in
-        removeCompositeNumbers n nums
+    removeCompositeNumbers n nums
+
 
 removeCompositeNumbers : Int -> List Int -> Bool
 removeCompositeNumbers n nums =
     case nums of
-        [] -> False
-        [x] -> n == x
-        x::xs ->
-            Debug.log "list" (List.filter (\num -> modBy x num /= 0) xs)
-                |> removeCompositeNumbers n
+        [] ->
+            False
 
--- Works! but not with big numbers. Need to make some optimizations.
+        [ x ] ->
+            n == x
+
+        x :: xs ->
+            -- If the last member of list exists, remove composite numbers. Otherwise, return false.
+            if List.drop (List.length xs - 1) xs == [ n ] then
+                Debug.log "list" (List.filter (\num -> modBy x num /= 0) xs)
+                    |> removeCompositeNumbers n
+            else
+                False
+
 
 main : Html.Html a
 main =
@@ -39,18 +51,18 @@ test : Int
 test =
     List.length <|
         List.filter (\( result, expect ) -> result /= expect)
-            [ ( isPrime 7, True )
-            -- , ( isPrime 10, False )
-            -- , ( isPrime -1, False )
-            -- , ( isPrime 1, False )
-            -- , ( isPrime 0, False )
-            -- , ( isPrime 120, False )
-            -- , ( isPrime 2, True )
-            -- , ( isPrime 36, False )
-            -- , ( isPrime 23, True )
-            -- , ( isPrime 6000, False )
-            -- , ( isPrime 7919, True )
-            -- , ( isPrime 7911, False )
+            [ ( isPrime 10, False )
+            , ( isPrime 7, True )
+            , ( isPrime -1, False )
+            , ( isPrime 1, False )
+            , ( isPrime 0, False )
+            , ( isPrime 120, False )
+            , ( isPrime 2, True )
+            , ( isPrime 36, False )
+            , ( isPrime 23, True )
+            , ( isPrime 6000, False )
+            , ( isPrime 7919, True )
+            , ( isPrime 7911, False )
             -- , ( isPrime 63247, True )
             , ( isPrime 63249, False )
             ]
