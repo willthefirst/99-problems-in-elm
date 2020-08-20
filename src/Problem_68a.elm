f5d92bd2952fe9fb35d7f63eab1def84af3c0c88
Will Moritz <will@crowdfavorite.com>
2020-08-20 21:15:41 +0200

    Finished 68a

--- src/Problem_68a.elm
+++ src/Problem_68a.elm
@@ -1,8 +1,10 @@
 module Problem_68a exposing (..)
+
 -- https://johncrane.gitbooks.io/ninety-nine-elm-problems/content/p/p68a.html
 
 import Html
 import List
+
 
 type Tree a
     = Empty
@@ -14,13 +16,16 @@ preorder tree =
     case tree of
         Empty ->
             []
-        Node a ->
+
+        Node a leftTree rightTree ->
+            a :: preorder leftTree ++ preorder rightTree
 
 
 main =
     Html.text
-        (if (test) then
+        (if test then
             "Your implementation passed all tests."
+
          else
             "Your implementation failed at least one test."
         )
@@ -29,44 +34,56 @@ main =
 test : Bool
 test =
     List.all ((==) True)
-      [ preorder Empty == []
-      , preorder tree67 == ['a', 'b', 'd', 'e', 'c', 'f', 'g'] 
-    --   , preorder tree68 == (List.range 1 9) 
-    --   , preorder tree69 == [6, 2, 1, 4, 3, 5, 7] 
-      ]
+        [ preorder Empty == []
+        , preorder tree67 == [ 'a', 'b', 'd', 'e', 'c', 'f', 'g' ]
+        , preorder tree68 == (List.range 1 9)
+        , preorder tree69 == [6, 2, 1, 4, 3, 5, 7]
+        ]
 
 
-tree67 = 
+tree67 =
     Node 'a'
         (Node 'b'
             (Node 'd' Empty Empty)
-            (Node 'e' Empty Empty))
-        (Node 'c' 
-            Empty 
-            (Node 'f' 
-                (Node 'g' Empty Empty) 
-                Empty))
+            (Node 'e' Empty Empty)
+        )
+        (Node 'c'
+            Empty
+            (Node 'f'
+                (Node 'g' Empty Empty)
+                Empty
+            )
+        )
 
-tree68 = 
+
+tree68 =
     Node 1
         (Node 2
             (Node 3 Empty Empty)
-            (Node 4 Empty Empty))
-        (Node 5 
-            Empty 
-            (Node 6 
-                (Node 7 
-                    Empty 
-                    (Node 8 
-                        (Node 9 Empty Empty) 
-                        Empty))
-                Empty))
+            (Node 4 Empty Empty)
+        )
+        (Node 5
+            Empty
+            (Node 6
+                (Node 7
+                    Empty
+                    (Node 8
+                        (Node 9 Empty Empty)
+                        Empty
+                    )
+                )
+                Empty
+            )
+        )
 
-tree69 = 
+
+tree69 =
     Node 6
         (Node 2
             (Node 1 Empty Empty)
-            (Node 4 
-                (Node 3 Empty Empty) 
-                (Node 5 Empty Empty)))
+            (Node 4
+                (Node 3 Empty Empty)
+                (Node 5 Empty Empty)
+            )
+        )
         (Node 7 Empty Empty)
